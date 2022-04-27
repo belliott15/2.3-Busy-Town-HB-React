83ed6e298454cb36ddd987e984c44a8f8515b754
Beau Elliott <belliott1516@gmail.com>
2022-04-27 16:08:08 -0700

    corrected issues to deploy to netlify

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import VehicleList from './VehicleList';
-import TrafficLight from './TrafficLight';
+// import TrafficLight from './TrafficLight';
 
 function App() {
   // track the following state with a few useState hooks:
@@ -33,7 +33,7 @@ function App() {
           </div>
         </div>
       </div>
-      <TrafficLight color={lightColor} />
+      {/* <TrafficLight color={lightColor} /> */}
       <div className="buttons">
         {/* when you click this button, the color of the light in state should be set to 'red' */}
         <button>Red</button>
